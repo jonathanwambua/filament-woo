072bbffa84f1df7b7165910a085480bac2b0d924
wambua-pesapal <jonathan.wambua@pesapal.com>
2024-07-16 16:21:46 +0300

    initial

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Customer;
+use App\Models\LeadSource;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'admin@admin.com',
+        ]);
+
+        Customer::factory()
+            ->count(10)
+            ->create();
+
+        $leadSources = [
+            'Website', 'Online AD', 'Twitter', 'LinkedIn', 'Webinar', 'Trade Show', 'Referral'
+        ];
+        foreach ($leadSources as $leadSource) {
+            LeadSource::create(['name' => $leadSource]);
+        }
+    }
+}
